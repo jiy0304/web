79a565f7b4778078c0eda0c777ec09e6e2c80982
inyoung jeong <48130766+jiy0304@users.noreply.github.com>
2024-06-04 03:21:53 +0900

    espoir

---
+++ espoir/js/main.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+// 메인슬라이드
+const mv = new Swiper(".mv", {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+    pagination: {
+        el: "#main_visual .swiper-pagination",
+        type: "progressbar",
+        clickable: true,
+    },
+    loop: true,
+});
+
+
+
+
+
+
